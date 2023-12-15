48ded13c6d1376a0a4400717fd998adb4809bbe8
sulibaShen <138857741+sulibaShen@users.noreply.github.com>
2023-12-15 10:43:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import TextMessage, MessageEvent, TextSendMessage
+from linebot.models import *
+
+import tempfile, os
+import datetime
+import openai
+import time
+import traceback
 
 app = Flask(__name__)
-
-# 请替换为您的Channel Access Token和Channel Secret
-line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('CHANNEL_SECRET')
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 def process_message(text):
     # 添加自定义条件，当用户输入"123"时，返回"321"
@@ -21,25 +28,38 @@ def process_message(text):
         # 在这里可以添加其他处理逻辑，例如调用GPT-3等
         return "机器人：我听不懂你在说什么！"
 
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
     reply_message = process_message(user_message)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
 if __name__ == "__main__":
-    # 运行Flask应用程序
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
